2ff69c34021b56f49a7aafd57ebf109c5ea114c7
Aguaments <50241853+Aguaments@users.noreply.github.com>
2022-09-14 10:24:31 +0800

    Update compare_strings.go

--- section3/3.12/compare_strings.go
+++ section3/3.12/compare_strings.go
@@ -5,37 +5,37 @@ import(
 )
 
 func compare_string(s1, s2 string) bool{
-        flag := 0
-        if len(s1) != len(s2){
-                return false
-        }else{
-                for _, c1 := range s1 {
-                        for _, c2 := range s2 {
-                                if c1 == c2{
-                                        flag = 0
-                                        break
-                                }else{
-                                        flag = 1
-                                }
-                        }
-                        if flag == 1{
-                                return false
-                        }
+		flag := 0
+    if len(s1) != len(s2){
+        return false
+    }else{
+        for _, c1 := range s1 {
+						for _, c2 := range s2 {
+            		if c1 == c2{
+                    flag = 0
+                    break
+                }else{
+                    flag = 1
                 }
-                return true
+           }
+           if flag == 1{
+           		return false
+           }
         }
+        return true
+   }
 
 }
 
 func main(){
-        s1 := "abce"
-        s2 := "ebca"
+		s1 := "abce"
+    s2 := "ebca"
         
-        if compare_string(s1, s2){
-                fmt.Println("True")
-        }else{
-                fmt.Println("False")
-        }
+    if compare_string(s1, s2){
+           fmt.Println("True")
+    }else{
+           fmt.Println("False")
+    }
 }
 
 
